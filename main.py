c311fbf0e52476a21304df5f66d51ff1ad558f53
Filip Vnenčák <vnencak.filip@gmail.com>
2022-04-25 09:45:00 +0200

    Mac

--- main.py
+++ main.py
@@ -2,24 +2,31 @@ import psycopg2
 import requests
 import json
 
-#conn = psycopg2.connect(user='docker', password='docker', host='127.0.0.1', port='5432')
-conn = psycopg2.connect("dbname='postgres' user='postgres' host='localhost' password='docker' port='5432'")
 
 with open('demo_data.json') as f:
     req = json.loads(f.read())
 
-
-
+connection = psycopg2.connect(  user="postgres",
+                                password="docker",
+                                host="127.0.0.1",
+                                port="5432",
+                                database="postgres")
 #print(json.dumps(data, sort_keys=True, indent=4))
 
-cur = conn.cursor()
-cur.execute( "CREATE TABLE test (id serial PRIMARY KEY, num integer, data varchar);")
+cur = connection.cursor()
+
+cur.execute("CREATE TABLE test (id serial PRIMARY KEY, num integer, data varchar);")
 
 
+connection.commit()
+# closing database connection.
+if connection:
 
-cur.close()
-conn.close()
-
+    cur.close()
+    connection.close()
+    print("PostgreSQL connection is closed")
+else:
+    print("No connection")
 
 
 
